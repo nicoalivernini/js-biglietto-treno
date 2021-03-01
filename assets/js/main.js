e1d38789e895da3febd825902a68e33779826cdd
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-01 16:17:28 +0100

    Dichiarazione scontistica

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,20 @@ var prezzoChilometro = 0.21;
 var prezzoBiglietto = kmPasseggero * prezzoChilometro;
 
 
+//Prezzo biglietto under 18
 
-//var prezzoOver65 = prezzoBiglietto - 40%;
+var calcoloBigliettoUnder18 = (prezzoBiglietto / 100 * 20);
+
+var prezzoBigliettoUnder18 = prezzoBiglietto - calcoloBigliettoUnder18;
+
+
+//Prezzo biglietto under 65
+
+var calcoloBigliettoOver65 = (prezzoBiglietto / 100 * 40);
+
+var prezzoBigliettoOver65 = prezzoBiglietto - calcoloBigliettoOver65;
+
+
 
 if (etaPasseggero < 18) {
 
