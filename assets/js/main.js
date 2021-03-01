42b5f7df5804928df97706af67719aa5bc0422f8
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-01 15:58:12 +0100

    Dichiarazione variabili

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+//Chilometri del passeggero
+var kmPasseggero = prompt("Quanti chilometri vuoi percorrere?");
+
+//Età del passeggero
+var etaPasseggero = prompt("Quanti anni hai?");
+
+//Prezzo del biglietto a chilometro
+var prezzoChilometro = 0.21;
+
+//Prezzo del biglietto in base ai chilometri inseriti
+var prezzoBiglietto = kmPasseggero * prezzoChilometro;
+
+
+
+//var prezzoOver65 = prezzoBiglietto - 40%;
+
+if (etaPasseggero < 18) {
+
+} else {
+  if (etaPasseggero > 65) {
+
+  }
+}
+
+console.log(kmPasseggero, etaPasseggero, prezzoBiglietto);
